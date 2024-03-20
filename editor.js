e80459bcd314c8591a35b8eb3277a6c45f4aceee
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-03-20 10:55:24 +0530

    added editor.js and seperating html and js

---
+++ editor.js
@@ -0,0 +1,164 @@
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("container-content");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+//List of fontlist
+let fontList = [
+  "Arial",
+  "Verdana",
+  "Times New Roman",
+  "Garamond",
+  "Georgia",
+  "Courier New",
+  "cursive",
+];
+
+//Initial Settings
+const initializer = () => {
+  //function calls for highlighting buttons
+  //No highlights for link, unlink,lists, undo,redo since they are one time operations
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButtons, true);
+
+  //create options for font names
+  fontList.map((value) => {
+    let option = document.createElement("option");
+    option.value = value;
+    option.innerHTML = value;
+    fontName.appendChild(option);
+  });
+
+  //fontSize allows only till 7
+  for (let i = 1; i <= 7; i++) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerHTML = i;
+    fontSizeRef.appendChild(option);
+  }
+
+  //default size
+  fontSizeRef.value = 3;
+};
+
+//main logic
+const modifyText = (command, defaultUi, value) => {
+  //execCommand executes command on selected text
+  document.execCommand(command, defaultUi, value);
+};
+
+//For basic operations which don't need value parameter
+optionsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modifyText(button.id, false, null);
+  });
+});
+
+//options that require value parameter (e.g colors, fonts)
+advancedOptionButton.forEach((button) => {
+  button.addEventListener("change", () => {
+    modifyText(button.id, false, button.value);
+  });
+});
+
+//link
+linkButton.addEventListener("click", () => {
+  let userLink = prompt("Enter a URL");
+  //if link has http then pass directly else add https
+  if (/http/i.test(userLink)) {
+    modifyText(linkButton.id, false, userLink);
+  } else {
+    userLink = "http://" + userLink;
+    modifyText(linkButton.id, false, userLink);
+  }
+});
+
+//Highlight clicked button
+const highlighter = (className, needsRemoval) => {
+  className.forEach((button) => {
+    button.addEventListener("click", () => {
+      //needsRemoval = true means only one button should be highlight and other would be normal
+      if (needsRemoval) {
+        let alreadyActive = false;
+
+        //If currently clicked button is already active
+        if (button.classList.contains("active")) {
+          alreadyActive = true;
+        }
+
+        //Remove highlight from other buttons
+        highlighterRemover(className);
+        if (!alreadyActive) {
+          //highlight clicked button
+          button.classList.add("active");
+        }
+      } else {
+        //if other buttons can be highlighted
+        button.classList.toggle("active");
+      }
+    });
+  });
+};
+
+const highlighterRemover = (className) => {
+  className.forEach((button) => {
+    button.classList.remove("active");
+  });
+};
+// function Export2Word(element, filename = "") {
+//   var preHtml =
+//     "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+//   var postHtml = "</body></html>";
+//   var html = preHtml + document.getElementById(element).innerHTML + postHtml;
+
+//   var blob = new Blob(["\ufeff", html], {
+//     type: "application/msword",
+//   });
+
+//   // Specify link url
+//   var url =
+//     "data:application/vnd.ms-word;charset=utf-8," + encodeURIComponent(html);
+
+//   // Specify file name
+//   filename = filename ? filename + ".docx" : "document.docx";
+
+//   // Create download link element
+//   var downloadLink = document.createElement("a");
+
+//   document.body.appendChild(downloadLink);
+
+//   if (navigator.msSaveOrOpenBlob) {
+//     navigator.msSaveOrOpenBlob(blob, filename);
+//   } else {
+//     // Create a link to the file
+//     downloadLink.href = url;
+
+//     // Setting the file name
+//     downloadLink.download = filename;
+
+//     //triggering the function
+//     downloadLink.click();
+//   }
+
+//   document.body.removeChild(downloadLink);
+// }
+window.onload = initializer();
+
+// function convertToDocx() {
+//   var FileSaver = require("file-saver");
+//   // Get HTML content from the div
+//   const htmlContent = document.getElementById("container-content").innerHTML;
+//   console.log(htmlContent);
+
+//   // Convert HTML to DOCX
+//   var converted = htmlDocx.asBlob(htmlContent);
+//   FileSaver.saveAs(converted, "test.docx");
+// }
---
+++ index.js
@@ -0,0 +1,356 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fileInput = document.querySelector("#files");
+  async function convertBlobToDataURL(blob, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      callback(e.target.result);
+    };
+    reader.readAsDataURL(blob);
+  }
+
+  async function convertDocxToBlob() {
+    const docxFilePath = "../g/sow2.docx";
+
+    try {
+      const response = await fetch(docxFilePath);
+      const arrayBuffer = await response.arrayBuffer();
+
+      const blob = new Blob([arrayBuffer], {
+        type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      });
+
+      return blob;
+    } catch (error) {
+      console.error("Error converting DOCX to Blob:", error);
+    }
+  }
+
+  async function renderDocx(file) {
+    try {
+      currentDocument = file;
+      if (!currentDocument) {
+        const docxOptions = Object.assign(docx.defaultOptions, {
+          debug: true,
+          experimental: true,
+
+          ignoreLastRenderedPageBreak: false,
+        });
+        var docData = await convertDocxToBlob();
+        await docx.renderAsync(
+          docData,
+          document.getElementById("container-content"),
+          null,
+          docxOptions
+        );
+      } else {
+        const docxOptions = Object.assign(docx.defaultOptions, {
+          debug: true,
+          experimental: true,
+
+          ignoreLastRenderedPageBreak: false,
+        });
+        var docData = await convertDocxToBlob();
+        await docx.renderAsync(
+          currentDocument,
+          document.getElementById("container-content"),
+          null,
+          docxOptions
+        );
+      }
+
+      const dynamicImages = document.querySelectorAll("img");
+      console.log(dynamicImages);
+
+      dynamicImages.forEach(function (image) {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        image.parentNode.insertBefore(container, image);
+        container.appendChild(image);
+
+        const resizeHandles = [
+          "top-left",
+          "top-right",
+          "bottom-left",
+          "bottom-right",
+        ].map(function (handleClass) {
+          const handle = document.createElement("div");
+          handle.classList.add("resize-handle", handleClass);
+          container.appendChild(handle);
+          return handle;
+        });
+
+        var tags = document.querySelectorAll("*");
+        var idCounter = 1;
+        tags.forEach(function (tag) {
+          if (!tag.id) {
+            tag.id = "id_" + idCounter;
+            idCounter++;
+          }
+        });
+
+        let initialX,
+          initialY,
+          offsetX = 0,
+          offsetY = 0;
+        let initialWidth, initialHeight;
+
+        // Event listener for mousedown event on the image and resize handles
+        image.addEventListener("mousedown", startDrag);
+        resizeHandles.forEach((handle) =>
+          handle.addEventListener("mousedown", startResize)
+        );
+
+        // Function to handle dragging
+        function startDrag(e) {
+          e.preventDefault();
+          initialX = e.clientX - offsetX;
+          initialY = e.clientY - offsetY;
+          document.addEventListener("mousemove", drag);
+          document.addEventListener("mouseup", stopDrag);
+        }
+
+        // Function to handle resizing
+        function startResize(e) {
+          e.preventDefault();
+          initialWidth = image.offsetWidth;
+          initialHeight = image.offsetHeight;
+          initialX = e.clientX;
+          initialY = e.clientY;
+          document.addEventListener("mousemove", resize);
+          document.addEventListener("mouseup", stopResize);
+        }
+
+        // Function to drag the image
+        function drag(e) {
+          e.preventDefault();
+          offsetX = e.clientX - initialX;
+          offsetY = e.clientY - initialY;
+          initialX = e.clientX;
+          initialY = e.clientY;
+          image.style.left = `${image.offsetLeft + offsetX}px`;
+          image.style.top = `${image.offsetTop + offsetY}px`;
+        }
+
+        // Function to resize the image
+        function resize(e) {
+          e.preventDefault();
+          const newWidth = initialWidth + (e.clientX - initialX);
+          const newHeight = initialHeight + (e.clientY - initialY);
+          image.style.width = `${newWidth}px`;
+          image.style.height = `${newHeight}px`;
+        }
+
+        // Functions to stop dragging and resizing
+        function stopDrag() {
+          document.removeEventListener("mousemove", drag);
+          document.removeEventListener("mouseup", stopDrag);
+        }
+
+        function stopResize() {
+          document.removeEventListener("mousemove", resize);
+          document.removeEventListener("mouseup", stopResize);
+        }
+      });
+
+      var sections = document.querySelectorAll(".docx");
+      console.log(sections);
+
+      sections.forEach(function (section) {
+        section.style.height = "910pt";
+        // section.style.padding = "130.05pt 72pt 72pt";
+
+        // Reset min-height if necessary
+        // section.style.maxHeight = "5855pt"; // Reset min-height if necessary
+      });
+    } catch (error) {
+      console.error("Error rendering DOCX:", error);
+    }
+  }
+  renderDocx();
+
+  fileInput.addEventListener("change", (ev) => {
+    renderDocx(fileInput.files[0]);
+    // testDocuments.selectedIndex = 0;
+  });
+});
+
+var currentNearestElement = null;
+function getNearestElementToCursor(cursorX, cursorY) {
+  const nearestElement = document.elementFromPoint(cursorX, cursorY);
+
+  if (nearestElement) {
+    // Remove highlight from previously highlighted element
+    const highlightedElement = document.querySelector(".highlight");
+    if (highlightedElement) {
+      highlightedElement.classList.remove("highlight");
+    }
+    // Highlight the nearest element
+    nearestElement.classList.add("highlight");
+    currentNearestElement = nearestElement;
+    console.log("changed");
+  }
+  return nearestElement;
+}
+
+function getNearestElementToCursorFromInput(cursorX, cursorY) {
+  const nearestElement = document.elementFromPoint(cursorX, cursorY);
+
+  if (nearestElement) {
+    // Remove highlight from previously highlighted element
+    const highlightedElement = document.querySelector(".highlight");
+    if (highlightedElement) {
+      highlightedElement.classList.remove("highlight");
+    }
+    // Highlight the nearest element
+    nearestElement.classList.add("highlight");
+    var editable = document.getElementById(nearestElement.id);
+    var lastText = editable.textContent;
+    console.log("inside", editable);
+
+    editable.addEventListener("input", function (event) {
+      var newText = this.textContent;
+      console.log("inside new", newText);
+
+      if (
+        event.inputType === "deleteContentBackward" ||
+        event.inputType === "deleteContentForward"
+      ) {
+        for (var i = 0; i < lastText.length - 1; i++) {
+          if (lastText[i] !== newText[i]) {
+            console.log("char '" + lastText[i] + "' was removed at index " + i);
+            lastText = newText;
+            return;
+          }
+        }
+      }
+    });
+  }
+  return nearestElement;
+}
+
+function getPosition(e) {
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  return {
+    x,
+    y,
+  };
+}
+
+function getCursorLocation(event) {
+  console.log(event);
+  if (event.key === "Backspace" || event.key === "Delete") {
+    // If it is, return without doing anything
+    console.log("here");
+    return;
+  }
+  const position = getPosition(event);
+  //   console.log("position", position);
+  var rect;
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    rect = range.getBoundingClientRect();
+    // console.log(
+    //   "Cursor X position:",
+    //   rect.left,
+    //   "Cursor Y position:",
+    //   rect.top
+    // );
+    getNearestElementToCursor(rect.left, rect.top);
+  }
+  return rect;
+}
+
+function getCursorLocationFromInput(event) {
+  const position = getPosition(event);
+  //   console.log("position", position);
+  var rect;
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    rect = range.getBoundingClientRect();
+    // console.log(
+    //   "Cursor X position:",
+    //   rect.left,
+    //   "Cursor Y position:",
+    //   rect.top
+    // );
+    getNearestElementToCursorFromInput(rect.left, rect.top);
+  }
+  return rect;
+}
+
+document
+  .getElementById("container-content")
+  .addEventListener("mouseup", getCursorLocation);
+document
+  .getElementById("container-content")
+  .addEventListener("keyup", getCursorLocation);
+const editableDiv = document.getElementById("container-content");
+let textData = [];
+
+editableDiv.addEventListener("input", function (event) {
+  const rect = getCursorLocationFromInput(event);
+  const nearestElement = getNearestElementToCursorFromInput(
+    rect.left,
+    rect.top
+  );
+  console.log(nearestElement.textContent, "vvvvv");
+  //   console.log("xxxx", event);
+  if (event.inputType == "insertText") {
+    const insertedText = event.data;
+
+    const selection = window.getSelection();
+    // console.log("xx", selection);
+    const range = selection.getRangeAt(0);
+    const startOffset = range.startOffset;
+    const endOffset = range.endOffset;
+
+    const position = {
+      insertedText: insertedText,
+      nearestElement: nearestElement.id,
+      contentlength: nearestElement.textContent.length,
+      startOffset: startOffset,
+      endOffset: endOffset,
+    };
+
+    textData.push(position);
+    console.log("Text inserted:", position);
+
+    // Store textData in JSON format
+    const jsonData = JSON.stringify(textData);
+    console.log("JSON Data:", jsonData);
+  } else if (event.inputType === "deleteContentBackward") {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const startOffset = range.startOffset;
+    const endOffset = range.endOffset;
+    const deletedText = range.toString();
+    console.log(range);
+    console.log(selection);
+
+    // const deletedText = range.cloneContents();
+    // const textNodes = Array.from(deletedText.childNodes).filter(
+    //   (child) => child.nodeName === "#text"
+    // );
+
+    // console.log("Deleted Text:", textNodes);
+
+    const position = {
+      deletedText: deletedText,
+      nearestElement: nearestElement.id,
+      contentlength: nearestElement.textContent.length,
+      startOffset: startOffset,
+      endOffset: endOffset,
+    };
+
+    textData.push(position);
+    console.log("Text deleted:", position);
+
+    // Store textData in JSON format
+    const jsonData = JSON.stringify(textData);
+    console.log("JSON Data:", jsonData);
+  }
+});

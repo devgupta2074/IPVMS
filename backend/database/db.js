49da13cb50fcf930930b26b7552bb10bb2685ecb
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-03-28 15:12:24 +0530

    UPDATE : New DB Connection (Now Working)

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,10 @@
 import pg from "pg";
 const { Pool } = pg;
-
 export const pool = new Pool({
-  user: "admin",
-  password: "root",
-  host: "localhost",
+  user: "root",
+  password: "xsRLlDQJnsniVeJTF1MUDbUCZxyXSQQl",
+  host: "dpg-co2h7bgl6cac73bnjcpg-a.singapore-postgres.render.com",
   port: 5432,
-  database: "test_db",
+  database: "ipvms",
+  ssl: true,
 });

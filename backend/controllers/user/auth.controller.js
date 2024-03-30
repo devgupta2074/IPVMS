2bc12b9203b5596a41a145870e4fb0cf127b8d10
Tapasvi Arora <tarora@EX2INDIA.LOCAL>
2024-03-30 19:26:50 +0530

    forgot password and auth middleware  via token added

--- backend/controllers/user/auth.controller.js
+++ backend/controllers/user/auth.controller.js
@@ -3,6 +3,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import path from "path";
+import { sendEmail } from "../../utils/Email/sendEmail.js";
+import { passwordValidation } from "../../utils/passwordValidation.js";
 const __dirname = path.resolve();
 dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
@@ -10,7 +12,7 @@ export const registerUser = async (req, res) => {
   try {
     // middleware apply user
     const { firstName, lastName, email, password, updatedBy } = req.body;
-    console.log(firstName, lastName, email, password);
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const user = await pool.query("SELECT * FROM puser WHERE email=$1", [
@@ -70,7 +72,7 @@ export const loginUser = async (req, res) => {
         .json({ success: false, message: "Invalid Password" });
     }
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRY_TIME,
     });
     const users = user.rows[0];
@@ -101,5 +103,173 @@ export const getAllUsers = async (req, res) => {
     return res
       .status(500)
       .json({ error: error, success: false, message: "Internal Server Error" });
+  }
+};
+export const forgotPassword = async (req, res) => {
+  //email validation above
+  const { email } = req.body;
+  try {
+    const user = await pool.query("SELECT * FROM puser WHERE email=$1", [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "user not found" });
+    }
+    //send email with token
+    const secretToken = jwt.sign(
+      { id: user.rows[0].id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    try {
+      await sendEmail(user.rows[0].email, secretToken);
+      return res.status(200).json({
+        success: true,
+        message: "Email sent",
+        secretToken: secretToken,
+      });
+    } catch (error) {
+      return res.status(502).json({
+        success: false,
+        message: "Bad Gateway error in sending email",
+      });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password, confirmPassword } = req.body;
+
+  if (!token) {
+    //401 unauthorized
+    //400 bad req ->invalid syntax
+    //402 payment required
+    //403 forbidden
+    //499 empty token
+    //498 token expired or invalid
+    return res.status(499).json({ success: false, message: "Invalid Token" });
+  }
+
+  try {
+    let verification;
+    try {
+      verification = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res
+        .status(498)
+        .json({ success: false, message: "Token either expired or invalid" });
+    }
+
+    if (!verification) {
+      return res
+        .status(498)
+        .json({ success: false, message: "Token either expired or invalid" });
+    }
+    const userId = verification.id;
+    const user = await pool.query("SELECT * FROM puser WHERE id=$1", [userId]);
+    if (user.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    if (!password) {
+      return res
+        .status(400)
+        .json({ message: "Password is required", success: false });
+    }
+
+    if (!passwordValidation(password).success) {
+      return res.status(400).json(passwordValidation(password));
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password and Confirm Password does not match",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const updatedUser = await pool.query(
+      "UPDATE puser SET password=$1 WHERE id=$2 RETURNING *",
+      [hashedPassword, userId]
+    );
+    const users = updatedUser.rows[0];
+    const token2 = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRY_TIME,
+    });
+    delete users["password"];
+    return res.status(200).json({
+      success: true,
+      message: "Password Reset Success",
+      token: token2,
+      user: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+export const resetPasswordAuth = async (req, res) => {
+  const userId = req.user.id;
+  const { password, confirmPassword } = req.body;
+  try {
+    const user = await pool.query("SELECT * FROM puser WHERE id=$1", [userId]);
+    if (user.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    if (!password) {
+      return res
+        .status(400)
+        .json({ message: "Password is required", success: false });
+    }
+
+    if (!passwordValidation(password).success) {
+      return res.status(400).json(passwordValidation(password));
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Password and Confirm Password does not match",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const updatedUser = await pool.query(
+      "UPDATE puser SET password=$1 WHERE id=$2 RETURNING *",
+      [hashedPassword, userId]
+    );
+    const users = updatedUser.rows[0];
+    const token2 = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRY_TIME,
+    });
+    delete users["password"];
+    return res.status(200).json({
+      success: true,
+      message: "Password Reset Success",
+      token: token2,
+      user: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+
+      message: "Internal Server Error",
+    });
   }
 };

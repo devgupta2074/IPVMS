081018608c098c68f98a74ab32b43f8736b7845f
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-04-05 12:41:17 +0530

    edit and delete of category apis

--- backend/controllers/catogery/catogery.controller.js
+++ backend/controllers/catogery/catogery.controller.js
@@ -52,13 +52,93 @@ export const getAllCategories = async (req, res) => {
             return res.status(200).json({
                 success: true,
                 message: "fetched catogeries",
-                data: data
+                count: data.rowCount,
+                data: data.rows
             });
         } else {
             return res
                 .status(400)
                 .json({ success: false, message: "No data found." });
         }
+
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(500)
+            .json({ error: error, success: false, message: "Something went wrong." });
+    }
+
+
+};
+
+
+export const editCategory = async (req, res) => {
+
+
+    try {
+        const body = req.body;
+
+        const query = {
+            text: "UPDATE category SET  category=($2) WHERE id=($1) RETURNING *",
+            values: [body.id, body.category]
+        };
+
+        const data = await pool.query(query);
+
+        if (data.rowCount != 0 | null) {
+
+            return res.status(200).json({
+                success: true,
+                message: "catogery updated",
+                count: data.rowCount,
+                data: data.rows
+            });
+        } else {
+            return res
+                .status(400)
+                .json({ success: false, message: "No data found." });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(500)
+            .json({ error: error, success: false, message: "Something went wrong." });
+    }
+
+
+};
+
+export const deleteCategory = async (req, res) => {
+
+    try {
+        const body = req.body;
+        const query = {
+            text: "UPDATE category SET is_active=FALSE  WHERE id=($1)",
+            values: [body.id]
+        };
+
+        const data = pool.query(query, (error, result) => {
+            if (error) {
+                return res.status(400).json({
+                    error: error.message,
+                    success: false,
+                });
+            }
+
+            if (result) {
+                return res.status(200).json({
+                    message: 'Category succesfully deleted', success: true
+                });
+            }
+        });
+        // console.log(data);
+
+        // res.status(200).json({
+        //     message: 'Category succesfully deleted', success: true
+        // });
+
+
 
     } catch (error) {
         console.log(error);
--- backend/routes/catogery.routes.js
+++ backend/routes/catogery.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createNewCategory, getAllCategories } from "../controllers/catogery/catogery.controller.js";
+import { createNewCategory, deleteCategory, editCategory, getAllCategories } from "../controllers/catogery/catogery.controller.js";
 
 
 const categoryRouter = express.Router();
@@ -7,6 +7,9 @@ const categoryRouter = express.Router();
 
 categoryRouter.post("/createNewCategory", createNewCategory);
 categoryRouter.get("/getAllCategories", getAllCategories);
+categoryRouter.patch("/editCategory", editCategory);
+categoryRouter.patch("/deleteCategory", deleteCategory);
+
 
 
 export default categoryRouter;

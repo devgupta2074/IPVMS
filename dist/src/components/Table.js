2057f58d197adf639c670bf8ea69163a067de071
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-05-16 14:10:39 +0530

    modal backdrop close

--- dist/src/components/Table.js
+++ dist/src/components/Table.js
@@ -11,7 +11,7 @@ const docxModal = (id) => {
     <div id=${id}  >
     <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20  sm:block sm:p-0 ">
       <!-- Background overlay -->
-      <div  class="fixed inset-0 bg-gray-900 bg-opacity-60 transition-opacity " aria-hidden="true"></div>
+      <div id="backdrop" class="fixed inset-0 bg-gray-900 bg-opacity-60 transition-opacity backdrop-dev " aria-hidden="true"></div>
   
       <!-- Modal content -->
       <div class="fixed inset-0  w-4/5 h-full pt-10 pb-10  m-auto  bg-white rounded-lg shadow-xl  transform transition-all sm:my-8 overflow-y-scroll">
@@ -559,8 +559,19 @@ function addModalOpenCloseFeature() {
       document.getElementById(modalId).innerHTML = contents;
     });
     await fetchAndRenderDoc(modalId);
-  };
 
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      var modal = document.getElementsByClassName("backdrop-dev");
+
+      console.log(event.target, "dd", modal, modalId);
+      for (var i = 0; i < modal.length; i++) {
+        if (event.target == modal[i]) {
+          document.getElementById(modalId).style.display = "none";
+        }
+      }
+    };
+  };
   window.closeModal = function (modalId) {
     document.getElementById(modalId).style.display = "none";
     document

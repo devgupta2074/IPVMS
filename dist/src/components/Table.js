4913172852285eba45004d3924a8f8f5fb36c917
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-05-27 12:46:39 +0530

    modal fixed

--- dist/src/components/Table.js
+++ dist/src/components/Table.js
@@ -17,19 +17,22 @@ const docxModal = (id) => {
       <div  class="fixed inset-0 bg-gray-900 bg-opacity-60 transition-opacity backdrop " aria-hidden="true"></div>
   
       <!-- Modal content -->
-      <div class="fixed inset-0  w-3/5 h-full pt-10 pb-10  m-auto  bg-white rounded-lg shadow-xl  transform transition-all sm:my-8 overflow-y-scroll">
-        <div class="absolute top-0 right-0 p-2 ">
-          <button onclick="closeModal(${id})" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-            </svg>
-          </button>
-        </div>
+      <div class="fixed  inset-0  w-3/5 h-full m-auto  bg-link-water-100 rounded-lg shadow-xl p-6 transform transition-all sm:my-8 overflow-y-scroll">
+       
   
-        <div id="printThis" class="p-6 pt-0  ">
-          <div id="render-docs" class="  flex flex-col justify-center items-center ">
+        <div id="printThis" class="  relative ">
+        <div class="relative ">
+        <button onclick="closeModal(${id})" type="button" class= " absolute top-0 right-0 p-1.5 m-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm  ml-auto inline-flex items-center">
+          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+          </svg>
+        </button>
+      </div>
+        
+          <div id="render-docs" class="  ">
+         
           ${style}
-          <div class='shadow-2xl' id='docx-wrapper'>
+          <div class='shadow-2xl bg-white' id='docx-wrapper'>
           </div>
           
             </div>

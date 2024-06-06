2b9f1fc85fa51bc389ab122758ab4fa159bed532
TAPASVI <Tapasviarora2002@gmail.com>
2024-06-06 10:33:52 +0530

    letter fixed

--- dist/src/scripts/loading.js
+++ dist/src/scripts/loading.js
@@ -2,14 +2,14 @@ export const showLoading = () => {
   const loading = document.createElement("div");
   loading.id = "loadingicon";
   loading.innerHTML = `<div id="loading"  >
-<div id="overlay" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.8); z-index: 1000;">
-<div class="flex gap-2 justify-center items-center h-screen">
-<div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
-<div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
-<div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
-</div>
-</div>  
-</div>`;
+  <div id="overlay" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.8); z-index: 1000;">
+  <div class="flex gap-2 justify-center items-center h-screen">
+  <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+  <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+  <div class="w-5 h-5 rounded-full animate-pulse bg-blue-600"></div>
+  </div>
+  </div>  
+  </div>`;
   document.body.appendChild(loading);
 };
 export const removeLoading = () => {

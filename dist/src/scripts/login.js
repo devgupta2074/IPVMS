5000ccd6a4384a6237c128e800a9bde8e89dbccc
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-06-11 16:22:03 +0530

    commit

--- dist/src/scripts/login.js
+++ dist/src/scripts/login.js
@@ -8,13 +8,6 @@ import {
 } from "../utils/constants.js";
 import { redirect, showNextPolicy } from "../utils/utils.js";
 
-let userdata;
-if (localStorage.getItem("token") === null) {
-  redirect(VIEWS_CONSTANTS.LOGIN);
-} else {
-  redirect(VIEWS_CONSTANTS.DASHBOARD);
-}
-
 const emailerror = document.getElementById("emailerror");
 const passworderror = document.getElementById("passworderror");
 
